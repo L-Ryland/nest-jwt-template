0d59d59ad3daf253dfae32e8b121ffbc8277af49
L-Ryland <saintleso@gmail.com>
2022-09-26 19:30:22 +0800

    feat: titles CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TitleModule } from './title/title.module';
+import { ConfigModule } from "@nestjs/config";
+import { KnexModule } from './knex/knex.module';
 
 @Module({
-  imports: [],
+  imports: [TitleModule, ConfigModule.forRoot({isGlobal: true}), KnexModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/dto/title.dto.ts
@@ -0,0 +1,15 @@
+import { Title } from "../interfaces/title.interface";
+
+export class TitleDto {
+  emp_no: number;
+  title: string;
+  from_date: Date;
+  to_date: Date;
+}
+
+export class QueryTitleDto extends TitleDto {
+  order?: {column: keyof TitleDto, order: "desc" | "asc"}[] = [];
+  pagination = 0;
+  limit? =  30;
+  offset? = (Number(this.pagination) + 1) * this.limit;
+}
